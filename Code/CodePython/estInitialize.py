a8e1a2d218e63545b15a69fae0511db2e91161b4
George Mac <griera@berkeley.edu>
2025-05-07 13:37:45 -0700

    REAL FINAL COMMIT I'M DONE!

--- Code/CodePython/estInitialize.py
+++ Code/CodePython/estInitialize.py
@@ -3,14 +3,7 @@ import scipy as sp
 #NO OTHER IMPORTS ALLOWED (However, you're allowed to import e.g. scipy.linalg)
 
 def estInitialize():
-    # Fill in whatever initialization you'd like here. This function generates
-    # the internal state of the estimator at time 0. You may do whatever you
-    # like here, but you must return something that is in the format as may be
-    # used by your estRun() function as the first returned variable.
-    #
-    # The second returned variable must be a list of student names.
-    # 
-    # The third return variable must be a string with the estimator type
+
     # Constants
     r = 0.425   # wheel radius
     B = 0.8     # Wheel base
@@ -21,18 +14,13 @@ def estInitialize():
     # Initial covariance
     P0 = np.diag([1.0, 1.0, 0.1])   # Have some initial uncertainty
 
-    # Process noise covariance  (TUNE)
-    #Q = np.diag([0.2, 0.1, 0.05])
-    Q = np.diag([1.0, 1.0, 0.25])
+    # Process noise covariance  (Values were tuned by hand)
+    Q = np.diag([0.2, 0.1, 0.05])
 
-    # Measurement noise covariance  (TUNE)
-   # R = np.diag([2.1, 2.0])
-    R = np.array([[1.09, 1.53], [1.53, 2.99]])  # Have some initial uncertainty
+    # Measurement noise covariance  (Values Were tuned by hand)
+    R = np.array([[1.00, 1.5], [1.5, 3.0]])  
 
-    # note that there is *absolutely no prescribed format* for this internal state.
-    # You can put in it whatever you like. Probably, you'll want to keep the position
-    # and angle, and probably you'll remove the color.
-    # Store internal state as dictionary
+    # Internal State
     internalState = [
         float(x0[0]),  # x
         float(x0[1]),  # y
